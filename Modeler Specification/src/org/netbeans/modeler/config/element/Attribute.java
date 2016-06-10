72dae3c92c39c494ff70947278c2d3ff15664d3b
gaurav.gupta <gaurav.gupta.jc@gmail.com>
2016-06-10 11:04:03 +0530

    Mapy Key UI Support

--- Modeler Specification/src/org/netbeans/modeler/config/element/Attribute.java
+++ Modeler Specification/src/org/netbeans/modeler/config/element/Attribute.java
@@ -191,31 +191,6 @@ public class Attribute {
         return "get" + name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 
-// public static String findGetterName(Class clazz, String name) throws IntrospectionException, NoSuchFieldException, NoSuchMethodException {
-//    Method getter = findGetter(clazz, name);
-//    if (getter == null) throw new NoSuchMethodException(clazz+" has no "+name+" getter");
-//    return getter.getName();
-//}
-//
-//public static Method findGetter(Class clazz, String name) throws IntrospectionException, NoSuchFieldException {
-//    BeanInfo info = Introspector.getBeanInfo(clazz);
-//    for ( PropertyDescriptor pd : info.getPropertyDescriptors() )
-//        if (name.equals(pd.getName())) return pd.getReadMethod();
-//    throw new NoSuchFieldException(clazz+" has no field "+name);
-//}
-//
-//public static String findSetterName(Class clazz, String name) throws IntrospectionException, NoSuchFieldException, NoSuchMethodException {
-//    Method setter = findSetter(clazz, name);
-//    if (setter == null) throw new NoSuchMethodException(clazz+" has no "+name+" setter");
-//    return setter.getName();
-//}
-//
-//public static Method findSetter(Class clazz, String name) throws IntrospectionException, NoSuchFieldException {
-//    BeanInfo info = Introspector.getBeanInfo(clazz);
-//    for ( PropertyDescriptor pd : info.getPropertyDescriptors() )
-//        if (name.equals(pd.getName())) return pd.getWriteMethod();
-//    throw new NoSuchFieldException(clazz+" has no field "+name);
-//}   
     /**
      * @return the groupId
      */
