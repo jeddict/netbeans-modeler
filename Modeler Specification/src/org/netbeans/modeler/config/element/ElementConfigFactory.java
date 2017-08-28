0a6c16f556d4cdb776d7bd308f312545a5548236
jGauravGupta <gaurav.gupta.jc@gmail.com>
2017-08-28 21:36:21 +0530

    Release v4.5 alpha

--- Modeler Specification/src/org/netbeans/modeler/config/element/ElementConfigFactory.java
+++ Modeler Specification/src/org/netbeans/modeler/config/element/ElementConfigFactory.java
@@ -54,7 +54,9 @@ public class ElementConfigFactory {
             if (element_TMP.getClassType() == null) {
                 throw new IllegalArgumentException(element_TMP.getId() + " class not found");
             }
-            if ((StringUtils.equals(category, element_TMP.getCategory()) || StringUtils.equals(category, element_TMP.getId())) && element_TMP.getClassType().isAssignableFrom(classType)) { //all super class
+            if ((StringUtils.equalsIgnoreCase(category, element_TMP.getCategory())
+                    || StringUtils.equalsIgnoreCase(category, element_TMP.getId()))
+                    && element_TMP.getClassType().isAssignableFrom(classType)) { //all super class
                 elements.add(element_TMP);
             }
         }
